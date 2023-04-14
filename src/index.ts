8fb08d739662b96d0dbca25cdb57dee4cf964837
grandmasterdev <one.ahmad.arifin@gmail.com>
2023-04-14 15:47:20 +0800

    updated action.yaml config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { context } from "@actions/github";
+import { setOutput } from "@actions/core";
 
 export const run = () => {
-  console.log('context ref: ', context.ref)
-};
+  const refParts = context.ref.split("/");
+  const branchName = refParts[refParts.length - 1];
+  
+  console.log("context ref: ", context.ref);
+  console.log("branch name: ", branchName );
 
+  setOutput('branch-name', branchName)
+};
 
 run();
